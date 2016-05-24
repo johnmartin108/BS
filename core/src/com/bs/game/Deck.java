7e07221dc702aa120ec0274fceb0624496b24cdd
John <Jmr7in108@gmail.com>
2016-05-24 15:18:37 -0400

    migrating deck and player stuff over

---
+++ core/src/com/bs/game/Deck.java
@@ -0,0 +1,37 @@
+package com.example.johnmartin.bs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by johnmartin on 5/19/16.
+ */
+public class Deck {
+    private ArrayList<Card> cards;
+
+//    private final String[] suits = new String[]{"Spades", "Clubs", "Hearts", "Diamonds"};
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 14; j++) {
+                this.cards.add(new Card(Card.toSuit(i), Card.toRank(j)));
+            }
+        }
+        shuffle();
+
+    }
+
+    public void shuffle() {
+        Collections.shuffle(this.cards);
+    }
+
+    public Card nextCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        else {
+            return cards.remove(0);
+        }
+    }
+}
---
+++ core/src/com/bs/game/Player.java
@@ -0,0 +1,28 @@
+package com.example.johnmartin.bs;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johnmartin on 5/20/16.
+ */
+public class Player {
+    private ArrayList<Card> hand;
+
+    public Player() {
+        hand = new ArrayList<Card>();
+    }
+
+    public void takeTurn() {
+        //display dialog with options
+        //if call BS
+            //get result, refresh options (including update info from server)
+        //else
+            //process play normally, send info to server
+    }
+
+    //helper function for dealing/taking cards
+    public void addToHand(ArrayList<Card> c) {
+        hand.addAll(c);
+    }
+
+}

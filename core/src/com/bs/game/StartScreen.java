f1e2c240cba895a677c66752af981d9c383f7d42
Nick Moolenijzer <nick@moolenijzer.com>
2016-05-31 16:21:54 -0400

    added vibration

--- core/src/com/bs/game/StartScreen.java
+++ core/src/com/bs/game/StartScreen.java
@@ -52,9 +52,9 @@ public class StartScreen implements Screen {
 
         game.batch.begin();
 
-        bitFont.draw(game.batch, "List of Players Joined", 350, 1350);
+        bitFont.draw(game.batch, "List of Players Joined:", 350, 1050);
         bitFont.draw(game.batch, "Press Start when ready", 350, 1200);
-        bitFont.draw(game.batch, "Host Status: "+game.hostStatus.toLowerCase(), 350, 1050);
+        bitFont.draw(game.batch, "Host Status: "+game.hostStatus.toLowerCase(), 350, 1350);
         for (int i = 0; i < game.peerlist.size(); i ++){
             String devName = (String)game.peerlist.get(i);
             bitFont.draw(game.batch, devName, 450, 900-i*100);
